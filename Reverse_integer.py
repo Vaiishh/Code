142eb336007293abd51cf1dd264dfa3633f6d88e
Vaish <vaish@Vaishs-MacBook-Air.local>
2025-10-29 13:19:47 -0700

    Reverse Integer

---
+++ Reverse_integer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = -1 if x < 0 else 1
+        a=str(abs(x))
+        reverse=a[::-1]
+        result = sign * int (reverse)
+
+        if result < -2**31 or result > 2**31-1 :
+            return 0
+        return result
+
+
+
+
+        
